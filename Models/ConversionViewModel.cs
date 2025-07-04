0abb71b062f0f34a84e20be10ac07c5d95c09e42
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-04 12:27:22 +0200

    added model

---
+++ Models/ConversionViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace UnitConverterAngular.Models
+{
+    public enum ConversionType
+    {
+        Area,
+        Length,
+        Volume,
+        Temperature,
+        Weight
+    }
+
+    public class ConversionViewModel
+    {
+        public double InputValue { get; set; }
+        public string FromUnit { get; set; }
+        public string ToUnit { get; set; }
+        public double? Result { get; set; }
+        public ConversionType ConversionType { get; set; }
+    }
+}
