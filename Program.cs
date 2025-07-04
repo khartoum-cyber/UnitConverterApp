470f4b68990e37235b8890f4f515c9e172c6dad1
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-04 12:25:12 +0200

    setup for Angular app

--- Program.cs
+++ Program.cs
@@ -9,15 +9,23 @@ namespace UnitConverterAngular
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddSpaStaticFiles(
+                configuration =>
+                {
+                    configuration.RootPath =
+                        "ClientApp/dist";
+                });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
