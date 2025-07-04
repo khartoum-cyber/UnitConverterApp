b29351ed9cdf38aa30b7525bacdfd5723c92fb93
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-04 14:04:58 +0200

    added controller for conversions

---
+++ Controllers/ConversionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitConverterAngular.Controllers
+{
+    public class ConversionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
