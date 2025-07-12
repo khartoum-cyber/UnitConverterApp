48707eeb1d49d1c6123d3821be8b0e7c1db4f461
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-12 09:30:33 +0200

    nested dictionary as primary data structure

--- Services/UnitConversionService.cs
+++ Services/UnitConversionService.cs
@@ -2,5 +2,28 @@
 {
     public class UnitConversionService
     {
+
+        private readonly Dictionary<string, Dictionary<string, Func<double, double>>> _conversions =
+            new Dictionary<string, Dictionary<string, Func<double, double>>>
+            {
+                ["Length"] = new Dictionary<string, Func<double, double>>
+                {
+                    ["ft->cm"] = value => value * 30.48,
+                    ["cm->ft"] = value => value / 30.48,
+                    ["m->km"] = value => value / 1000,
+                    ["km->m"] = value => value * 1000
+                },
+                ["Weight"] = new Dictionary<string, Func<double, double>>
+                {
+                    ["kg->lb"] = value => value * 2.20462,
+                    ["lb->kg"] = value => value / 2.20462
+                },
+                ["Temperature"] = new Dictionary<string, Func<double, double>>
+                {
+                    ["c->f"] = value => (value * 9 / 5) + 32,
+                    ["f->c"] = value => (value - 32) * 5 / 9
+                }
+            };
+
     }
 }
