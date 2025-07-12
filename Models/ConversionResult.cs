1e365b131e904a5fe6bf022940363755de3cf2da
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-12 09:24:41 +0200

    conversion response fields

--- Models/ConversionResult.cs
+++ Models/ConversionResult.cs
@@ -2,5 +2,9 @@
 {
     public class ConversionResult
     {
+        public double OriginalValue { get; set; }
+        public string FromUnit { get; set; }
+        public double ConvertedValue { get; set; }
+        public string ToUnit { get; set; }
     }
 }
