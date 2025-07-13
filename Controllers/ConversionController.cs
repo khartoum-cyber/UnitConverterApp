3d191a3e367d59fdf353bbdbaef073363c97b39a
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-07-13 16:35:26 +0200

    conversion controller with direct dependency of unitConversionService

--- Controllers/ConversionController.cs
+++ Controllers/ConversionController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UnitConverterAngular.Models;
+using UnitConverterAngular.Services;
 
 namespace UnitConverterAngular.Controllers
 {
-    public class ConversionController : Controller
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConversionController : ControllerBase
     {
-        public IActionResult Index()
+        //private readonly UnitConversionService _conversionService;
+
+        //public ConversionController(UnitConversionService conversionService)
+        //{
+        //    _conversionService = conversionService;
+        //}
+
+        private readonly UnitConversionService _conversionService = new UnitConversionService();
+
+        [HttpPost]
+        public ActionResult<ConversionResult> Convert([FromBody] ConversionRequest request)
         {
-            return View();
+            try
+            {
+                var result = _conversionService.Convert(request.Category, request.FromUnit, request.ToUnit, request.Value);
+                return Ok(new ConversionResult
+                {
+                    OriginalValue = request.Value,
+                    FromUnit = request.FromUnit,
+                    ConvertedValue = result,
+                    ToUnit = request.ToUnit
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
