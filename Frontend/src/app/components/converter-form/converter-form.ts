43987edf0d0382c2cfe374f704a2d0c1a3e9d1e9
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-08-06 14:14:37 +0200

    tab switching on units works

--- Frontend/src/app/components/converter-form/converter-form.ts
+++ Frontend/src/app/components/converter-form/converter-form.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-converter-form',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './converter-form.html',
   styleUrl: './converter-form.css'
 })
 export class ConverterForm {
-
+  @Input() selected!: string;
 }
--- Frontend/src/app/components/tabs/tabs.ts
+++ Frontend/src/app/components/tabs/tabs.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConverterForm } from '../converter-form/converter-form';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.html',
   styleUrls: ['./tabs.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, ConverterForm]
 })
 export class TabsComponent {
   // Array of tab names
