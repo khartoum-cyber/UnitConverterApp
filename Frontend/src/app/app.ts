3d547e164f4de6207826af98ea87d0c348bcfd80
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-08-06 10:34:15 +0200

    logic and styles for tabs

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabsComponent } from './components/tabs/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('UnitConverterCLientApp');
+  protected readonly title = signal('UnitConverterClientApp');
 }
--- Frontend/src/app/components/tabs/tabs.ts
+++ Frontend/src/app/components/tabs/tabs.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
   templateUrl: './tabs.html',
-  styleUrl: './tabs.css'
+  styleUrls: ['./tabs.css'],
+  imports: [CommonModule]
 })
-export class Tabs {
+export class TabsComponent {
+  // Array of tab names
+  tabs: string[] = ['Length', 'Weight', 'Temperature'];
+  // Currently selected tab
+  selectedTab: string = this.tabs[0];
 
+  // Method to switch tabs
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
 }
