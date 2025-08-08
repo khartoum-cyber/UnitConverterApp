6401677830df6dd5d391d7d6d146fd014a8a216b
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-08-08 14:34:42 +0200

    converter-form - layout and logic changes

--- Frontend/src/app/components/converter-form/converter-form.ts
+++ Frontend/src/app/components/converter-form/converter-form.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
@@ -11,7 +11,7 @@ interface Units {
 
 @Component({
   selector: 'app-converter-form',
-  imports: [CommonModule, ReactiveFormsModule, MatSelectModule, MatFormFieldModule],
+  imports: [ReactiveFormsModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './converter-form.html',
   styleUrl: './converter-form.css'
 })
@@ -19,20 +19,19 @@ export class ConverterForm {
   @Input() selected!: string;
   
   unitFrom = "";
+  unitTo = "";
 
   converterForm = new FormGroup({
     number: new FormControl(0),
-    convertFrom: new FormControl(''),
-    convertTo: new FormControl(''),
   });
 
   lengthUnits: Units[] = [
-    {value: 'mm-0', viewValue: 'Milimeters'},
-    {value: 'cm-1', viewValue: 'Centimeters'},
-    {value: 'm-2', viewValue: 'Meters'},
+    {value: 'mm', viewValue: 'Milimeters'},
+    {value: 'cm', viewValue: 'Centimeters'},
+    {value: 'm', viewValue: 'Meters'},
   ];
 
     handleSubmit() {
-    alert(this.unitFrom + ' | ' + this.converterForm.value.convertTo);
+    alert(this.converterForm.value.number + ' FROM ' + this.unitFrom + ' TO ' + this.unitTo);
   }
 }
