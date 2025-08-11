e453dfe4fe2148654eb65a8c2fcdb3624c1a905e
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-08-11 21:19:47 +0200

    http post logic

--- Frontend/src/app/components/converter-form/converter-form.ts
+++ Frontend/src/app/components/converter-form/converter-form.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormControl, FormGroup } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { HttpClient } from '@angular/common/http';
 
 interface ConversionRequest {
   number: number;
@@ -18,11 +19,14 @@ interface Units {
 
 @Component({
   selector: 'app-converter-form',
-  imports: [ ReactiveFormsModule, MatSelectModule, MatFormFieldModule],
+  imports: [ ReactiveFormsModule, MatSelectModule, MatFormFieldModule ],
   templateUrl: './converter-form.html',
   styleUrl: './converter-form.css'
 })
 export class ConverterForm {
+  constructor(private http: HttpClient){
+  }
+
   @Input() selected!: 'Length' | 'Weight' | 'Temperature';
   
   get currentUnits() {
@@ -37,7 +41,7 @@ export class ConverterForm {
         return [];
     }
   }
-
+          
   unitFrom = "";
   unitTo = "";
 
@@ -62,7 +66,24 @@ export class ConverterForm {
     {value: 'f', viewValue: 'Fahrenheit'},
   ];
 
-    handleSubmit() {
-    alert(this.converterForm.value.number + ' FROM ' + this.unitFrom + ' TO ' + this.unitTo);
+    handleSubmit(): void {
+      const payload: ConversionRequest = {
+        number: this.converterForm.value.number ?? 0,
+        unitFrom: this.unitFrom,
+        unitTo: this.unitTo,
+        category: this.selected,
+    };
+
+    
+    this.http.post('http://localhost:5192/api/conversion', payload)
+        .subscribe({
+          next: response => {
+            console.log('Conversion result:', response);
+            // Optionally show result to user
+          },
+          error: err => {
+            console.error('Error during conversion:', err);
+          }
+        });
   }
 }
